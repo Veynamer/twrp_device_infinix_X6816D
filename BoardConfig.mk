171bed22489b95a3b2c7fca2720423754d9b4c79
Veynamer <113140575+Veynamer@users.noreply.github.com>
2024-03-28 14:30:13 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,6 +202,7 @@ TW_INCLUDE_FASTBOOTD := true
 TW_CUSTOM_CLOCK_POS := 47
 TW_CUSTOM_CPU_POS := 287
 TW_MTP_DEVICE := /dev/mtp_usb
+USE_RECOVERY_INSTALLER := true
 
 # Debug
 TWRP_EVENT_LOGGING := true
@@ -212,5 +213,4 @@ TARGET_USES_LOGD := true
 TW_LOAD_VENDOR_MODULES := "incrementalfs.ko kheaders.ko trace_irqsoff_bytedancy.ko trace_noschedule_bytedancy.ko trace_runqlat_bytedancy.ko adaptive_ts.ko"
 
 #PBRP_FLAGS
-#PB_TORCH_PATH := "/sys/devices/virtual/torch/torch/torch_level"
-#PB_TORCH_MAX_BRIGHTNESS := 1
+PB_TORCH_PATH := "/sys/devices/virtual/torch/torch/torch_level"
